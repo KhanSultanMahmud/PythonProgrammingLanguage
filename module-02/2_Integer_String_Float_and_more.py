871aa1d8ec4cb73c063b7d67ec2034c98f30924b
Khan Sultan Mahmud <sultancse1@gmail.com>
2025-12-09 10:41:23 +0600

    update

---
+++ module-02/2_Integer_String_Float_and_more.py
@@ -0,0 +1,66 @@
+x = 9
+y = 5
+
+sum1 = x + y
+diff1 = x - y
+prod1 = x * y
+quot1 = x / y
+floor_quot1 = x // y
+mod1 = x % y
+exp1 = x ** y
+print("Sum:", sum1)
+print("Difference:", diff1)
+print("Product:", prod1)
+print("Quotient:", quot1)
+print("Floor Quotient:", floor_quot1)
+print("Modulus:", mod1)
+print("Exponentiation:", exp1)
+str_x = str(x)
+str_y = str(y)
+concat_str = str_x + str_y
+print("Concatenated String:", concat_str)
+a=3.5
+b="2.5"
+sum2 = a + float(b)
+print("Sum of float and converted string:", sum2)
+diff2 = a - float(b)
+print("Difference of float and converted string:", diff2)
+prod2 = a * float(b)
+print("Product of float and converted string:", prod2)
+quot2 = a / float(b)
+print("Quotient of float and converted string:", quot2) 
+floor_quot2 = a // float(b)
+print("Floor Quotient of float and converted string:", floor_quot2)
+mod2 = a % float(b)
+print("Modulus of float and converted string:", mod2)
+exp2 = a ** float(b)
+print("Exponentiation of float and converted string:", exp2)
+str_a = str(a)
+str_b = str(b)
+concat_str2 = str_a + str_b
+print("Concatenated String of float and string:", concat_str2)
+print("Type of x:", type(x))
+print("Type of y:", type(y))
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of str_x:", type(str_x))
+print("Type of str_y:", type(str_y))
+print("Type of str_a:", type(str_a))
+print("Type of str_b:", type(str_b))
+print("Type of concat_str:", type(concat_str))
+print("Type of concat_str2:", type(concat_str2))
+# Additional operations between integer and float
+sum3 = x + a
+print("Sum of integer and float:", sum3)
+diff3 = x - a
+print("Difference of integer and float:", diff3)
+prod3 = x * a
+print("Product of integer and float:", prod3)
+quot3 = x / a
+print("Quotient of integer and float:", quot3)
+floor_quot3 = x // a
+print("Floor Quotient of integer and float:", floor_quot3)
+mod3 = x % a
+print("Modulus of integer and float:", mod3)
+exp3 = x ** a
+print("Exponentiation of integer and float:", exp3) 
